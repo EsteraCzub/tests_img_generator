47073441fea10acc1b6c4182b309f58bac8c81d7
Estera Czub <39551762+EsteraCzub@users.noreply.github.com>
2023-04-18 13:28:57 +0200

    Cleaning code: visual form
    Makes code the easiest to read

--- cypress/e2e/history.cy.js
+++ cypress/e2e/history.cy.js
@@ -7,28 +7,40 @@ describe('history module', () => {
 	})
 
 	it('display the history', () => {
-		cy.get('div').should('have.class', 'ant-list-split')
+		cy.get('div')
+			.should('have.class', 'ant-list-split')
 	})
 
 	it('display details on the image', () => {
-		cy.get('img').first().click()
-		cy.get('div[data-testid="imgDiv"]').should('be.visible')
+		cy.get('img')
+			.first()
+			.click()
 
-		cy.get('img[data-testid="openImg"]').should('have.css', 'width', '512px')
-		cy.get('img[data-testid="openImg"]').should('have.css', 'height', '512px')
+		cy.get('div[data-testid="imgDiv"]')
+			.should('be.visible')
+
+		cy.get('img[data-testid="openImg"]')
+			.should('have.css', 'width', '512px')
+		cy.get('img[data-testid="openImg"]')
+			.should('have.css', 'height', '512px')
 
 		cy.get(':nth-child(2) > .ant-descriptions-item-content')
-		.should('have.text', 'portrait of a beautiful girl with scorpions around her, slight smile, digital painting, concept art, sharp focus, illustration, au naturel, hyper detailed, digital art, trending in artstation, cinematic lighting, studio quality, smooth render, unreal engine 5')
+			.should('have.text', 'portrait of a beautiful girl with scorpions around her, slight smile, digital painting, concept art, sharp focus, illustration, au naturel, hyper detailed, digital art, trending in artstation, cinematic lighting, studio quality, smooth render, unreal engine 5')
 
-		cy.get(':nth-child(4) > :nth-child(1) > span').should('have.text', '512x512')
+		cy.get(':nth-child(4) > :nth-child(1) > span')
+			.should('have.text', '512x512')
 
-		cy.get(':nth-child(4) > :nth-child(2) > span').should('have.text', '10')
+		cy.get(':nth-child(4) > :nth-child(2) > span')
+			.should('have.text', '10')
 		
-		cy.get(':nth-child(4) > :nth-child(3) > span').should('have.text', '7.5')
+		cy.get(':nth-child(4) > :nth-child(3) > span')
+			.should('have.text', '7.5')
 		
-		cy.get(':nth-child(4) > :nth-child(4) > span').should('have.text', '985117537')
+		cy.get(':nth-child(4) > :nth-child(4) > span')
+			.should('have.text', '985117537')
 		
-		cy.get(':nth-child(4) > :nth-child(5) > span').should('have.text', 'stable_tf')
+		cy.get(':nth-child(4) > :nth-child(5) > span')
+			.should('have.text', 'stable_tf')
 
 		cy.get('div[data-testid="imgDiv"]').click()
 	})
--- cypress/e2e/main.cy.js
+++ cypress/e2e/main.cy.js
@@ -10,85 +10,127 @@ describe('main window tests', () => {
 	it('test switch theme', () => {
 		cy.get('button[data-testid="switch"]').click()
 
-		cy.get('div[id="app"]').should('have.class', 'theme-light')
+		cy.get('div[id="app"]')
+			.should('have.class', 'theme-light')
 	})
 
 	it('test the left menu', () => {
-		cy.get('li[data-testid="leftMenu1"]').click()
-		.should('contain.html', '/text2image')
+		cy.get('li[data-testid="leftMenu1"]')
+			.click()
+			.should('contain.html', '/text2image')
 
-		cy.get('li[data-testid="leftMenu2"]').click()
-		.should('contain.html', '/history')
+		cy.get('li[data-testid="leftMenu2"]')
+			.click()
+			.should('contain.html', '/history')
 	})
 
 	it('test the sidebar menu', () => {
 		cy.get('button[data-testid="selectMenu"]').click()
-		cy.get('.ant-drawer-wrapper-body')
-		.should('not.have.class', 'ant-drawer-content-wrapper-hidden')
 
-		cy.get('li[data-testid="menu1"]').click()
-		.should('contain.html', '/text2image')
+		cy.get('.ant-drawer-wrapper-body')
+			.should('not.have.class', 'ant-drawer-content-wrapper-hidden')
+
+		cy.get('li[data-testid="menu1"]')
+			.click()
+			.should('contain.html', '/text2image')
 
 		cy.get('button[data-testid="selectMenu"]').click()
-		cy.get('li[data-testid="menu2"]').click()
-		.should('contain.html', '/profile')
+
+		cy.get('li[data-testid="menu2"]')
+			.click()
+			.should('contain.html', '/profile')
 	})
 
 	it('test the login box, valid data', () => {
 		cy.get('li[data-testid="login"]').click()
-		cy.get('.ant-modal-content').should('be.visible')
 
-		cy.get('input[id="username"]').type('Test Testerski')
-		cy.get('input[id="password"]').type('bardzoBezpieczneHasło123!')
+		cy.get('.ant-modal-content')
+			.should('be.visible')
+
+		cy.get('input[id="username"]')
+			.type('Test Testerski')
+
+		cy.get('input[id="password"]')
+			.type('bardzoBezpieczneHasło123!')
 
 		cy.get('button[data-testid="loginButton"]').click()
 
-		cy.get('li[data-testid="logout"]').should('be.visible')
+		cy.get('li[data-testid="logout"]')
+			.should('be.visible')
 	})
 
 	it('test the login box, without data', () => {
 		cy.get('li[data-testid="login"]').click()
-		cy.get('.ant-modal-content').should('be.visible')
 
-		cy.get('input[id="username"]').type('{enter}')
-		cy.get('div[id="username_help"]').should('be.visible')
+		cy.get('.ant-modal-content')
+			.should('be.visible')
 
-		cy.get('input[id="password"]').type('{enter}')
-		cy.get('div[id="password_help"]').should('be.visible')
+		cy.get('input[id="username"]')
+			.type('{enter}')
+
+		cy.get('div[id="username_help"]')
+			.should('be.visible')
+
+		cy.get('input[id="password"]')
+			.type('{enter}')
+
+		cy.get('div[id="password_help"]')
+			.should('be.visible')
 	})
 
 	it('test the register box, valid data', () => {
 		cy.get('li[data-testid="register"]').click()
-		cy.get('.ant-modal-content').should('be.visible')
 
-		cy.get('input[id="username"]').type('Test Testerski')
-		cy.get('input[id="password"]').type('bardzoBezpieczneHasło123!')
+		cy.get('.ant-modal-content')
+			.should('be.visible')
+
+		cy.get('input[id="username"]')
+			.type('Test Testerski')
+		
+		cy.get('input[id="password"]')
+			.type('bardzoBezpieczneHasło123!')
 
 		cy.get('button[data-testid="registerButton"]').click()
 
-		cy.get('li[data-testid="logout"]').should('be.visible')
+		cy.get('li[data-testid="logout"]')
+			.should('be.visible')
 	})
 	
 	it('test the register box, without data', () => {
 		cy.get('li[data-testid="register"]').click()
-		cy.get('.ant-modal-content').should('be.visible')
+		
+		cy.get('.ant-modal-content')
+			.should('be.visible')
 
-		cy.get('input[id="username"]').type('{enter}')
-		cy.get('div[id="username_help"]').should('be.visible')
+		cy.get('input[id="username"]')
+			.type('{enter}')
+		
+		cy.get('div[id="username_help"]')
+			.should('be.visible')
 
-		cy.get('input[id="password"]').type('{enter}')
-		cy.get('div[id="password_help"]').should('be.visible')
+		cy.get('input[id="password"]')
+			.type('{enter}')
+		
+		cy.get('div[id="password_help"]')
+			.should('be.visible')
 	})
 
 	it('test logout', () => {
 		cy.get('li[data-testid="login"]').click()
 
-		cy.get('input[id="username"]').type('Test Testerski')
-		cy.get('input[id="password"]').type('bardzoBezpieczneHasło123!{enter}')
+		cy.get('input[id="username"]')
+			.type('Test Testerski')
+
+		cy.get('input[id="password"]')
+			.type('bardzoBezpieczneHasło123!{enter}')
 
 		cy.get('li[data-testid="logout"]').click()
-		cy.get('li[data-testid="login"]').should('be.visible')
-		cy.get('li[data-testid="register"]').should('be.visible')
+		
+		cy.get('li[data-testid="login"]')
+			.should('be.visible')
+			
+		cy.get('li[data-testid="register"]')
+			.should('be.visible')
 	})
 
 })
